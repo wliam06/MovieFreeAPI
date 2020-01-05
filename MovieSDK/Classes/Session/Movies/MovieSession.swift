1ecb33e15b7dc76f8c5abc3438c66d05949b113b
William <will.liiam06@gmail.com>
2020-01-05 14:12:27 +0700

    fix: remove latest endpoint

--- MovieSDK/Classes/Session/Movies/MovieSession.swift
+++ MovieSDK/Classes/Session/Movies/MovieSession.swift
@@ -10,24 +10,13 @@ import Foundation
 open class Movies {
   public init() {}
 
-  /// Get the most newly created movie
-  /// - Parameter completion: Movie Response contains success and failed response
-  open func latestMovie(completion: @escaping CompletionHandler) {
-    ServiceManager.api.load(urlPath: MovieListPath.latest.path, method: .GET) { (data, error) in
-      if let error = error {
-        completion(.failure(error))
-        return
-      }
-//      completion(.success(data))
-    }
-  }
 
   /// List of movies based on status ( Now Playing, Popular, Top Rated, Upcoming )
   /// - Parameter movie: Movie status
   /// - Parameter page (optional): Movie list of page, by default page is `1`
   /// - Parameter language (optional): Movie language description, by default language is `en-US`
   /// - Parameter completion: Movie response contains success and failed (error)
-  open func listOfMove(movie: MovieListPath, page: String = "1", language: String = "en-US",
+  open func listOfMovie(movie: MovieListPath, page: String = "1", language: String = "en-US",
                        completion: @escaping CompletionHandler) {
     let param: [String: String] = ["language": language, "page": page]
 
@@ -47,12 +36,14 @@ open class Movies {
         if let dict = movieResponse.dictionary {
           completion(.success(dict))
         }
+
       } catch {
         completion(.failure(ServiceError(type: .invalid)))
       }
     }
   }
 
+  
   /// Detail of Movie
    /// - Parameters:
    ///   - id: Movie id
@@ -61,6 +52,7 @@ open class Movies {
    open func detailOfMovie(withID id: Int, language: String = "en-US", completion: @escaping CompletionHandler) {
      let param: [String: String] = ["language": language]
      let path = MovieDetailPath.movieDetail(movieID: id).path
+
      ServiceManager.api.load(urlPath: path, queryItems: param, method: .GET, body: nil) { (data, error) in
        if let error = error {
          completion(.failure(error))
